ac3c39571d325685115c012a79d56404f92a0ff2
lurenjia528 <816837404@qq.com>
2020-04-09 21:59:21 +0800

    prometheus监控golang

---
+++ prometheus/metrics.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
