c5a5ceee373a8c3e6eeed5802b053d70635c7e8f
yanggt <yanggt@163.com>
2020-04-09 17:33:56 +0800

    添加sa

--- k8sclientincluster/client.go
+++ k8sclientincluster/client.go
@@ -30,7 +30,7 @@ func main() {
 		// Examples for error handling:
 		// - Use helper functions like e.g. errors.IsNotFound()
 		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("kube-system").Get("calico-node-8cc5n", metav1.GetOptions{})
+		_, err = clientset.CoreV1().Pods("kube-system").Get("calico-node-8mf84", metav1.GetOptions{})
 		if errors.IsNotFound(err) {
 			fmt.Printf("Pod not found\n")
 		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
